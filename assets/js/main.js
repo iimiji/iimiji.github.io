434c63e14ec5264ec7dfc34b7d97b4f06b68aa17
iimiji <mijik@sfu.ca>
2022-12-07 00:01:43 -0800

    overall + styleguide

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,7 +180,29 @@ const sr = ScrollReveal({
   // reset: true // Animations repeat
 })
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 sr.reveal(`.home__data, .projects__container, .testimonial__container, .footer__container`)
+sr.reveal(`.root, .explain, .root-text`, {delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'})
 sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'})
